635f08408a8edc34fa42ad44a5641d308a18f4fb
Chau Tsun Man <tsunman.chau96@gmail.com>
2017-03-03 14:50:31 -0800

    Connect to the database

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function connectDB() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      alert(this.responseText);
+    }
+  };
+  xmlhttp.open("GET", "test.php", true);
+  xmlhttp.send();
+}
