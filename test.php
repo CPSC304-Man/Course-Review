ad6023ef5001f427cda66138c630b6076c7e0c3c
Chau Tsun Man <tsunman.chau96@gmail.com>
2017-03-03 14:09:21 -0800

    Access the database

--- test.php
+++ test.php
@@ -1,8 +1,7 @@
-
 <html>
 <?php
 
-if ($c=OCILogon("Username", "Password", "ug")) {
+if ($c=OCILogon("ora_b8f1b", "a28144970", "ug")) {
   echo "Successfully connected to Oracle.\n";
   OCILogoff($c);
 } else {
