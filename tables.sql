c8983bf33c88a6b82dcf70422d2152cedbdf2c12
Chau Tsun Man <tsunman.chau96@gmail.com>
2017-03-11 15:30:39 -0800

    Populate the database

--- tables.sql
+++ tables.sql
@@ -96,3 +96,37 @@ CREATE TABLE Recommend (
     CONSTRAINT Recomend_FK_professor FOREIGN KEY (professor_ID) REFERENCES Professor(professor_ID),
     CONSTRAINT Recomend_FK_admin FOREIGN KEY (admin_ID) REFERENCES Admin(admin_ID)
 );
+
+
+INSERT INTO Professor VALUES ('P00000001', 'Professor 1');
+INSERT INTO Professor VALUES ('P00000002', 'Professor 2');
+INSERT INTO Professor VALUES ('P00000003', 'Professor 3');
+INSERT INTO Professor VALUES ('P00000004', 'Professor 4');
+INSERT INTO Professor VALUES ('P00000005', 'Professor 5');
+
+INSERT INTO Student VALUES ('28144970', 'CHAU Tsun Man', 'tmchau@connect.ust.hk');
+INSERT INTO Student VALUES ('00000001', 'Student 1', 'student1@ubc.ca');
+INSERT INTO Student VALUES ('00000002', 'Student 2', 'student2@ubc.ca');
+INSERT INTO Student VALUES ('00000003', 'Student 3', 'student3@ubc.ca');
+INSERT INTO Student VALUES ('00000004', 'Student 4', 'student4@ubc.ca');
+INSERT INTO Student VALUES ('00000005', 'Student 5', 'student5@ubc.ca');
+
+INSERT INTO TA VALUES ('TA00000001', 'TA 1');
+INSERT INTO TA VALUES ('TA00000002', 'TA 2');
+INSERT INTO TA VALUES ('TA00000003', 'TA 3');
+INSERT INTO TA VALUES ('TA00000004', 'TA 4');
+INSERT INTO TA VALUES ('TA00000005', 'TA 5');
+
+INSERT INTO Admin VALUES ('CPSCAdmin', 'CPSC');
+INSERT INTO Admin VALUES ('COMMAdmin', 'COMM');
+
+INSERT INTO Course VALUES ('CPSC304_2016W2', 'CPSC', '304', '2016 Winter Term 2', 'P00000001', 'TA00000001', '80.0');
+INSERT INTO Course VALUES ('CPSC304_2016W1', 'CPSC', '313', '2016 Winter Term 1', 'P00000001', 'TA00000001', '80.0');
+INSERT INTO Course VALUES ('CPSC313_2016W2', 'CPSC', '317', '2016 Winter Term 2', 'P00000002', 'TA00000002', '70.0');
+INSERT INTO Course VALUES ('CPSC317_2016W2', 'CPSC', '294', '2016 Winter Term 2', 'P00000003', 'TA00000003', '60.0');
+INSERT INTO Course VALUES ('COMM294_2016W2', 'COMM', '304', '2016 Winter Term 2', 'P00000004', 'TA00000004', '50.0');
+
+INSERT INTO CourseDetails VALUES ('CPSC', '304', 'Introduction to Relational Databases', 'Introduction to Relational Databases', 'CPSCAdmin', NULL);
+INSERT INTO CourseDetails VALUES ('CPSC', '313', 'Computer Hardware and Operating Systems', 'Computer Hardware and Operating Systems', 'CPSCAdmin', NULL);
+INSERT INTO CourseDetails VALUES ('CPSC', '317', 'Internet Computing', 'Internet Computing', 'CPSCAdmin', NULL);
+INSERT INTO CourseDetails VALUES ('COMM', '294', 'Managerial Accounting', 'Managerial Accounting', 'COMMAdmin', NULL);
