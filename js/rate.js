7138c53e6ef9fd07d07025e8e397258f5a593d7b
Chau Tsun Man <tsunman.chau96@gmail.com>
2017-03-17 14:53:25 -0700

    Rate a course

--- js/rate.js
+++ js/rate.js
@@ -1,5 +1,8 @@
-var user = getUser(function(userId) {
-    console.log(userId);
+var userID;
+getUser(function(userID) {
+    console.log(userID);
+
+    userID = userID;
 });
 // TODO: authenticate the user
 
@@ -9,16 +12,36 @@ console.log(course_id);
 
 
 function rate() {
+    var courseComment = $('#course-comment')[0].value;
+    var courseRate = $('#course-rate-input')[0].value;
+    var professorComment = $('#professor-comment')[0].value;
+    var professorRate = $('#professor-rate-input')[0].value;
+    var taComment = $('#ta-comment')[0].value;
+    var taRate = $('#ta-rate-input')[0].value;
+
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             console.log(JSON.parse(this.responseText));
 
-            window.open("course.html", "_self");
+            // window.open("course.html", "_self");
         }
     };
 
     xmlhttp.open('POST', 'rate.php', true);
     xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xmlhttp.send('course_id='+course_id);
+    xmlhttp.send(
+            'course_id='+course_id+
+            '&student_id='+userID+
+            '&courseComment='+courseComment+
+            '&courseRate='+courseRate+
+            '&professorComment='+professorComment+
+            '&professorRate='+professorRate+
+            '&taComment='+taComment+
+            '&taRate='+taRate);
+}
+
+
+function rateChange(rateID) {
+    $('#'+rateID+'-label').text($('#'+rateID+'-input')[0].value);
 }
