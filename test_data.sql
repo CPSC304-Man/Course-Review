a3077743b6d95fecfcaf95b46fbfe0ff847050c3
Rachel <rachel@fishman.org>
2017-03-23 08:03:58 -0700

    Create test_data.sql

---
+++ test_data.sql
@@ -0,0 +1,21 @@
+/*reviewcourse
+ReviewCourse(course_ID, rate_deadline, review_deadline)
+*/ 
+
+
+
+
+/*ratecourse
+RateCourse(rate_course_ID, course_ID, student_ID, courseComment, courseRate, professorComment, professorRate, TAComment, TARate)
+*/ 
+
+
+/*review
+Review(course_ID, professor_ID, comment)
+*/
+
+
+
+/*recommendation
+Recommend(course_ID, professor_ID, recommendation, admin_ID, complete)
+*/
