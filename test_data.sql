cfd2c378ab3c7c0a556b2e3f6816be16887807ba
Rachel <rachel@fishman.org>
2017-03-25 20:03:39 -0700

    Update test_data.sql
    Every student rated CPSC304_2016W2
    All courses were rated by student 00000002.
    
    More table entries to come.

--- test_data.sql
+++ test_data.sql
@@ -1,13 +1,143 @@
-/*reviewcourse
-ReviewCourse(course_ID, rate_deadline, review_deadline)
-*/ 
+
+INSERT INTO ReviewCourse VALUES('CPSC304_2016W2', '01-06-17', '01-06-27');
+INSERT INTO ReviewCourse VALUES('CPSC304_2016W1', '01-01-17', '01-06-27');
+INSERT INTO ReviewCourse VALUES('CPSC313_2016W2', '01-06-17', '01-06-27');
+INSERT INTO ReviewCourse VALUES('CPSC317_2016W2', '01-06-17', '01-06-27');
+INSERT INTO ReviewCourse VALUES('COMM294_2016W2', '01-06-17', '01-06-27');
+INSERT INTO ReviewCourse VALUES('CPSC110_2016W1', '01-01-17', '01-06-27');
+INSERT INTO ReviewCourse VALUES('CPSC121_2016W2', '01-06-17', '01-06-27');
+INSERT INTO ReviewCourse VALUES('CPSC121_2015W2', '01-06-16', '01-06-27');
 
 
 
 
-/*ratecourse
-RateCourse(rate_course_ID, course_ID, student_ID, courseComment, courseRate, professorComment, professorRate, TAComment, TARate)
-*/ 
+INSERT INTO RateCourse VALUES('80000001', 'CPSC304_2016W2', '28144970', 'The course was amazing.  I loved learning about relational databases. The course was informative and challenging', '8', 'Hazra is amazing!', '8', 'TAs are very helpful in lab and on Piazza.', '7');
+INSERT INTO RateCourse VALUES('80000002', 'CPSC304_2016W2', '00000001', 'I loved learning about databases. The course was informative and challenging', '9', 'She is resourceful and confident in teaching. She understands the students in difficulty, respects them, creative and professional. I am happy that she is really my professor.', '9', 'TAs are OK.', '3');
+INSERT INTO RateCourse VALUES('80000003', 'CPSC304_2016W2', '00000002', 'The assignments were not that easy, and I messed up the quizzes and exam as well.', '2', 'But overall, a good teacher ... cares for student and very patient.', '9', 'TAs are good.', '9');
+INSERT INTO RateCourse VALUES('80000004', 'CPSC304_2016W2', '00000003', 'The course was OK not amazing', '5', 'Very nice teacher and gives good grades to all the students.', '9', 'TAs are good.', '6');
+INSERT INTO RateCourse VALUES('80000005', 'CPSC304_2016W2', '00000004', 'course was hard.', '9', 'professor was easy to understand.', '9', 'ta was ok.', '3');
+INSERT INTO RateCourse VALUES('80000006', 'CPSC304_2016W2', '00000005', 'course was easy.', '1', 'professor cared about the students.', '10', 'ta was mediocore.', '1');
+INSERT INTO RateCourse VALUES('80000007', 'CPSC304_2016W2', '00000006', 'course was boring.', '2', 'professor cared about the content.', '1', 'ta was useless.', '2');
+INSERT INTO RateCourse VALUES('80000008', 'CPSC304_2016W2', '00000007', 'course was exciting.', '3', 'professor was uncommunicative.', '2', 'ta was helpful.', '9');
+INSERT INTO RateCourse VALUES('80000009', 'CPSC304_2016W2', '00000008', 'course was cool.', '4', 'professor was funny.', '3', 'ta was mean.', '3');
+INSERT INTO RateCourse VALUES('80000010', 'CPSC304_2016W2', '00000009', 'course was applicable to real life.', '5', 'professor was engaging.', '4', 'ta was nice.', '8');
+INSERT INTO RateCourse VALUES('80000011', 'CPSC304_2016W2', '00000010', 'course was informative.', '6', 'professor was nice.', '5', 'ta engaging.', '7');
+INSERT INTO RateCourse VALUES('80000012', 'CPSC304_2016W2', '00000011', 'course challengeing.', '7', 'professor was mean.', '6', 'ta was cool.', '6');
+INSERT INTO RateCourse VALUES('80000013', 'CPSC304_2016W2', '00000012', 'course was very very cool.', '10', 'professor made the course fun.', '7', 'ta was attractive.', '5');
+
+
+
+INSERT INTO RateCourse VALUES('80000014', 'CPSC304_2016W2', '10000001', 'course comment.', '9', 'professor comment.', '9', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000015', 'CPSC304_2016W2', '10000002', 'course comment.', '6', 'professor comment.', '9', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000016', 'CPSC304_2016W2', '10000003', 'course comment.', '5', 'professor comment.', '1', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000017', 'CPSC304_2016W2', '10000004', 'course comment.', '9', 'professor comment.', '1', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000018', 'CPSC304_2016W2', '10000005', 'course comment.', '9', 'professor comment.', '1', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000019', 'CPSC304_2016W2', '10000006', 'course comment.', '9', 'professor comment.', '2', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000020', 'CPSC304_2016W2', '10000007', 'course comment.', '4', 'professor comment.', '2', 'ta comment.', '5');
+INSERT INTO RateCourse VALUES('80000021', 'CPSC304_2016W2', '10000008', 'course comment.', '9', 'professor comment.', '2', 'ta comment.', '5');
+INSERT INTO RateCourse VALUES('80000022', 'CPSC304_2016W2', '10000009', 'course comment.', '2', 'professor comment.', '2', 'ta comment.', '5');
+INSERT INTO RateCourse VALUES('80000023', 'CPSC304_2016W2', '10000010', 'course comment.', '2', 'professor comment.', '9', 'ta comment.', '5');
+INSERT INTO RateCourse VALUES('80000024', 'CPSC304_2016W2', '10000011', 'course comment.', '9', 'professor comment.', '9', 'ta comment.', '5');
+INSERT INTO RateCourse VALUES('80000025', 'CPSC304_2016W2', '10000012', 'course comment.', '1', 'professor comment.', '9', 'ta comment.', '5');
+INSERT INTO RateCourse VALUES('80000026', 'CPSC304_2016W2', '10000013', 'course comment.', '1', 'professor comment.', '9', 'ta comment.', '5');
+INSERT INTO RateCourse VALUES('80000027', 'CPSC304_2016W2', '10000014', 'course comment.', '1', 'professor comment.', '9', 'ta comment.', '5');
+INSERT INTO RateCourse VALUES('80000028', 'CPSC304_2016W2', '10000015', 'course comment.', '9', 'professor comment.', '4', 'ta comment.', '5');
+INSERT INTO RateCourse VALUES('80000029', 'CPSC304_2016W2', '10000016', 'course comment.', '9', 'professor comment.', '4', 'ta comment.', '5');
+INSERT INTO RateCourse VALUES('80000030', 'CPSC304_2016W2', '10000017', 'course comment.', '9', 'professor comment.', '4', 'ta comment.', '5');
+INSERT INTO RateCourse VALUES('80000031', 'CPSC304_2016W2', '10000018', 'course comment.', '9', 'professor comment.', '4', 'ta comment.', '5');
+INSERT INTO RateCourse VALUES('80000032', 'CPSC304_2016W2', '10000019', 'course comment.', '6', 'professor comment.', '4', 'ta comment.', '4');
+INSERT INTO RateCourse VALUES('80000033', 'CPSC304_2016W2', '10000020', 'course comment.', '6', 'professor comment.', '4', 'ta comment.', '4');
+INSERT INTO RateCourse VALUES('80000034', 'CPSC304_2016W2', '10000021', 'course comment.', '6', 'professor comment.', '9', 'ta comment.', '4');
+INSERT INTO RateCourse VALUES('80000035', 'CPSC304_2016W2', '10000022', 'course comment.', '6', 'professor comment.', '9', 'ta comment.', '4');
+INSERT INTO RateCourse VALUES('80000036', 'CPSC304_2016W2', '10000023', 'course comment.', '6', 'professor comment.', '4', 'ta comment.', '4');
+INSERT INTO RateCourse VALUES('80000037', 'CPSC304_2016W2', '10000024', 'course comment.', '5', 'professor comment.', '9', 'ta comment.', '4');
+INSERT INTO RateCourse VALUES('80000038', 'CPSC304_2016W2', '10000025', 'course comment.', '5', 'professor comment.', '5', 'ta comment.', '4');
+INSERT INTO RateCourse VALUES('80000039', 'CPSC304_2016W2', '10000026', 'course comment.', '5', 'professor comment.', '5', 'ta comment.', '4');
+INSERT INTO RateCourse VALUES('80000040', 'CPSC304_2016W2', '10000027', 'course comment.', '5', 'professor comment.', '5', 'ta comment.', '3');
+INSERT INTO RateCourse VALUES('80000041', 'CPSC304_2016W2', '10000028', 'course comment.', '2', 'professor comment.', '5', 'ta comment.', '3');
+INSERT INTO RateCourse VALUES('80000042', 'CPSC304_2016W2', '10000029', 'course comment.', '2', 'professor comment.', '5', 'ta comment.', '3');
+INSERT INTO RateCourse VALUES('80000043', 'CPSC304_2016W2', '10000030', 'course comment.', '2', 'professor comment.', '9', 'ta comment.', '3');
+INSERT INTO RateCourse VALUES('80000044', 'CPSC304_2016W2', '10000031', 'course comment.', '2', 'professor comment.', '6', 'ta comment.', '3');
+INSERT INTO RateCourse VALUES('80000045', 'CPSC304_2016W2', '10000032', 'course comment.', '2', 'professor comment.', '6', 'ta comment.', '3');
+INSERT INTO RateCourse VALUES('80000046', 'CPSC304_2016W2', '10000033', 'course comment.', '1', 'professor comment.', '6', 'ta comment.', '3');
+INSERT INTO RateCourse VALUES('80000047', 'CPSC304_2016W2', '10000034', 'course comment.', '1', 'professor comment.', '6', 'ta comment.', '3');
+INSERT INTO RateCourse VALUES('80000048', 'CPSC304_2016W2', '10000035', 'course comment.', '1', 'professor comment.', '7', 'ta comment.', '2');
+INSERT INTO RateCourse VALUES('80000049', 'CPSC304_2016W2', '10000036', 'course comment.', '10', 'professor comment.', '7', 'ta comment.', '2');
+INSERT INTO RateCourse VALUES('80000050', 'CPSC304_2016W2', '10000037', 'course comment.', '10', 'professor comment.', '7', 'ta comment.', '2');
+INSERT INTO RateCourse VALUES('80000051', 'CPSC304_2016W2', '10000038', 'course comment.', '10', 'professor comment.', '7', 'ta comment.', '2');
+INSERT INTO RateCourse VALUES('80000052', 'CPSC304_2016W2', '10000039', 'course comment.', '10', 'professor comment.', '7', 'ta comment.', '2');
+INSERT INTO RateCourse VALUES('80000053', 'CPSC304_2016W2', '10000040', 'course comment.', '10', 'professor comment.', '7', 'ta comment.', '2');
+INSERT INTO RateCourse VALUES('80000054', 'CPSC304_2016W2', '10000041', 'course comment.', '9', 'professor comment.', '7', 'ta comment.', '2');
+INSERT INTO RateCourse VALUES('80000055', 'CPSC304_2016W2', '10000042', 'course comment.', '9', 'professor comment.', '7', 'ta comment.', '1');
+INSERT INTO RateCourse VALUES('80000056', 'CPSC304_2016W2', '10000043', 'course comment.', '5', 'professor comment.', '7', 'ta comment.', '1');
+INSERT INTO RateCourse VALUES('80000057', 'CPSC304_2016W2', '10000044', 'course comment.', '5', 'professor comment.', '7', 'ta comment.', '1');
+INSERT INTO RateCourse VALUES('80000058', 'CPSC304_2016W2', '10000045', 'course comment.', '5', 'professor comment.', '8', 'ta comment.', '1');
+INSERT INTO RateCourse VALUES('80000059', 'CPSC304_2016W2', '10000046', 'course comment.', '7', 'professor comment.', '8', 'ta comment.', '1');
+INSERT INTO RateCourse VALUES('80000060', 'CPSC304_2016W2', '10000047', 'course comment.', '3', 'professor comment.', '8', 'ta comment.', '1');
+INSERT INTO RateCourse VALUES('80000061', 'CPSC304_2016W2', '10000048', 'course comment.', '3', 'professor comment.', '8', 'ta comment.', '1');
+INSERT INTO RateCourse VALUES('80000062', 'CPSC304_2016W2', '10000049', 'course comment.', '9', 'professor comment.', '8', 'ta comment.', '1');
+INSERT INTO RateCourse VALUES('80000063', 'CPSC304_2016W2', '10000050', 'course comment.', '3', 'professor comment.', '8', 'ta comment.', '1');
+INSERT INTO RateCourse VALUES('80000064', 'CPSC304_2016W2', '10000051', 'course comment.', '3', 'professor comment.', '8', 'ta comment.', '1');
+INSERT INTO RateCourse VALUES('80000065', 'CPSC304_2016W2', '10000052', 'course comment.', '3', 'professor comment.', '6', 'ta comment.', '1');
+INSERT INTO RateCourse VALUES('80000066', 'CPSC304_2016W2', '10000053', 'course comment.', '3', 'professor comment.', '3', 'ta comment.', '1');
+INSERT INTO RateCourse VALUES('80000067', 'CPSC304_2016W2', '10000054', 'course comment.', '9', 'professor comment.', '3', 'ta comment.', '1');
+INSERT INTO RateCourse VALUES('80000068', 'CPSC304_2016W2', '10000055', 'course comment.', '9', 'professor comment.', '3', 'ta comment.', '1');
+INSERT INTO RateCourse VALUES('80000069', 'CPSC304_2016W2', '10000056', 'course comment.', '4', 'professor comment.', '3', 'ta comment.', '3');
+INSERT INTO RateCourse VALUES('80000070', 'CPSC304_2016W2', '10000057', 'course comment.', '4', 'professor comment.', '3', 'ta comment.', '3');
+INSERT INTO RateCourse VALUES('80000071', 'CPSC304_2016W2', '10000058', 'course comment.', '4', 'professor comment.', '3', 'ta comment.', '3');
+INSERT INTO RateCourse VALUES('80000072', 'CPSC304_2016W2', '10000059', 'course comment.', '4', 'professor comment.', '10', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000073', 'CPSC304_2016W2', '10000060', 'course comment.', '8', 'professor comment.', '9', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000074', 'CPSC304_2016W2', '10000061', 'course comment.', '8', 'professor comment.', '9', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000075', 'CPSC304_2016W2', '10000062', 'course comment.', '8', 'professor comment.', '9', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000076', 'CPSC304_2016W2', '10000063', 'course comment.', '8', 'professor comment.', '10', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000077', 'CPSC304_2016W2', '10000064', 'course comment.', '8', 'professor comment.', '10', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000078', 'CPSC304_2016W2', '10000065', 'course comment.', '7', 'professor comment.', '10', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000079', 'CPSC304_2016W2', '10000066', 'course comment.', '7', 'professor comment.', '10', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000080', 'CPSC304_2016W2', '10000067', 'course comment.', '7', 'professor comment.', '10', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000081', 'CPSC304_2016W2', '10000068', 'course comment.', '7', 'professor comment.', '10', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000082', 'CPSC304_2016W2', '10000069', 'course comment.', '1', 'professor comment.', '2', 'ta comment.', '8');
+INSERT INTO RateCourse VALUES('80000083', 'CPSC304_2016W2', '10000070', 'course comment.', '1', 'professor comment.', '2', 'ta comment.', '8');
+INSERT INTO RateCourse VALUES('80000084', 'CPSC304_2016W2', '10000071', 'course comment.', '5', 'professor comment.', '2', 'ta comment.', '8');
+INSERT INTO RateCourse VALUES('80000085', 'CPSC304_2016W2', '10000072', 'course comment.', '7', 'professor comment.', '2', 'ta comment.', '8');
+INSERT INTO RateCourse VALUES('80000086', 'CPSC304_2016W2', '10000073', 'course comment.', '7', 'professor comment.', '2', 'ta comment.', '8');
+INSERT INTO RateCourse VALUES('80000087', 'CPSC304_2016W2', '10000074', 'course comment.', '7', 'professor comment.', '9', 'ta comment.', '8');
+INSERT INTO RateCourse VALUES('80000088', 'CPSC304_2016W2', '10000075', 'course comment.', '7', 'professor comment.', '9', 'ta comment.', '8');
+INSERT INTO RateCourse VALUES('80000089', 'CPSC304_2016W2', '10000076', 'course comment.', '7', 'professor comment.', '6', 'ta comment.', '8');
+INSERT INTO RateCourse VALUES('80000091', 'CPSC304_2016W2', '10000077', 'course comment.', '7', 'professor comment.', '6', 'ta comment.', '8');
+INSERT INTO RateCourse VALUES('80000113', 'CPSC304_2016W2', '10000078', 'course comment.', '6', 'professor comment.', '6', 'ta comment.', '8');
+INSERT INTO RateCourse VALUES('80000092', 'CPSC304_2016W2', '10000079', 'course comment.', '6', 'professor comment.', '9', 'ta comment.', '9');
+INSERT INTO RateCourse VALUES('80000093', 'CPSC304_2016W2', '10000080', 'course comment.', '6', 'professor comment.', '8', 'ta comment.', '9');
+INSERT INTO RateCourse VALUES('80000094', 'CPSC304_2016W2', '10000081', 'course comment.', '6', 'professor comment.', '7', 'ta comment.', '9');
+INSERT INTO RateCourse VALUES('80000095', 'CPSC304_2016W2', '10000082', 'course comment.', '6', 'professor comment.', '7', 'ta comment.', '9');
+INSERT INTO RateCourse VALUES('80000096', 'CPSC304_2016W2', '10000083', 'course comment.', '6', 'professor comment.', '7', 'ta comment.', '9');
+INSERT INTO RateCourse VALUES('80000097', 'CPSC304_2016W2', '10000084', 'course comment.', '9', 'professor comment.', '7', 'ta comment.', '9');
+INSERT INTO RateCourse VALUES('80000098', 'CPSC304_2016W2', '10000085', 'course comment.', '9', 'professor comment.', '7', 'ta comment.', '9');
+INSERT INTO RateCourse VALUES('80000099', 'CPSC304_2016W2', '10000086', 'course comment.', '9', 'professor comment.', '7', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000100', 'CPSC304_2016W2', '10000087', 'course comment.', '6', 'professor comment.', '3', 'ta comment.', '9');
+INSERT INTO RateCourse VALUES('80000101', 'CPSC304_2016W2', '10000088', 'course comment.', '6', 'professor comment.', '4', 'ta comment.', '9');
+INSERT INTO RateCourse VALUES('80000102', 'CPSC304_2016W2', '10000089', 'course comment.', '9', 'professor comment.', '4', 'ta comment.', '7');
+INSERT INTO RateCourse VALUES('80000103', 'CPSC304_2016W2', '10000090', 'course comment.', '6', 'professor comment.', '4', 'ta comment.', '9');
+INSERT INTO RateCourse VALUES('80000104', 'CPSC304_2016W2', '10000091', 'course comment.', '9', 'professor comment.', '4', 'ta comment.', '9');
+INSERT INTO RateCourse VALUES('80000105', 'CPSC304_2016W2', '10000092', 'course comment.', '6', 'professor comment.', '4', 'ta comment.', '10');
+INSERT INTO RateCourse VALUES('80000106', 'CPSC304_2016W2', '10000093', 'course comment.', '9', 'professor comment.', '4', 'ta comment.', '10');
+INSERT INTO RateCourse VALUES('80000107', 'CPSC304_2016W2', '10000094', 'course comment.', '5', 'professor comment.', '5', 'ta comment.', '10');
+INSERT INTO RateCourse VALUES('80000108', 'CPSC304_2016W2', '10000095', 'course comment.', '5', 'professor comment.', '5', 'ta comment.', '10');
+INSERT INTO RateCourse VALUES('80000109', 'CPSC304_2016W2', '10000096', 'course comment.', '2', 'professor comment.', '5', 'ta comment.', '10');
+INSERT INTO RateCourse VALUES('80000110', 'CPSC304_2016W2', '10000097', 'course comment.', '2', 'professor comment.', '5', 'ta comment.', '10');
+INSERT INTO RateCourse VALUES('80000111', 'CPSC304_2016W2', '10000098', 'course comment.', '2', 'professor comment.', '9', 'ta comment.', '10');
+INSERT INTO RateCourse VALUES('80000112', 'CPSC304_2016W2', '10000099', 'course comment.', '10', 'professor comment.', '9', 'ta comment.', '10');
+
+
+INSERT INTO RateCourse VALUES('70000001', 'CPSC304_2016W1', '00000002', 'I love relational databases!', '10', 'I love Hazra!!', '10', 'I love 304 tas.', '10');
+INSERT INTO RateCourse VALUES('60000001', 'CPSC313_2016W2', '00000002', 'I love computer systems!', '9', 'Feely is amazing!', '9', '313 tas rule.', '9');
+INSERT INTO RateCourse VALUES('50000001', 'CPSC317_2016W2', '00000002', 'I love 317', '8', 'Acton is amazing!', '8', 'TAs are very helpful in lab and on Piazza.', '7');
+INSERT INTO RateCourse VALUES('40000001', 'COMM294_2016W2', '00000002', 'Yay Sauder', '8', 'Lopes Rogo is amazing!', '8', '294 TAs are very helpful.', '8');
+INSERT INTO RateCourse VALUES('30000001', 'CPSC110_2016W1', '00000002', 'I will forever trust the recurssion', '7', 'Gregor is scary!', '7', 'TAs are very helpful in lecture.', '7');
+INSERT INTO RateCourse VALUES('20000001', 'CPSC121_2016W2', '00000002', 'Breadboard 4ever', '6', 'Wolfman is amazing!', '6', 'TAs are very helpful in lab.', '6');
+INSERT INTO RateCourse VALUES('10000001', 'CPSC121_2015W2', '00000002', 'What the heck is a proof?!', '5', 'Patrice is amazing! He deserves an award', '8', 'My breadboard didnt like me.', '7');
+
+
+
 
 
 /*review
@@ -19,3 +149,4 @@ Review(course_ID, professor_ID, comment)
 /*recommendation
 Recommend(course_ID, professor_ID, recommendation, admin_ID, complete)
 */
+
